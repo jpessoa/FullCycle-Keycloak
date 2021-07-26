fb1ef7c5705f41dc9d9ce541ab7c81332e215dbd
Jefferson E. Pessoa - EasyNet Machine <jefferson@mile.com.br>
2021-07-26 12:55:49 -0400

    Antes da foto 2

--- app1/main.go
+++ app1/main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 
-	"github.com/coreos/go-oidc"
 	oidc "github.com/coreos/go-oidc"
 	"golang.org/x/oauth2"
 )
 
 var (
 	clientID     = "app"
-	clientSecret = "..."
+	clientSecret = "009895bf-1473-4215-bb04-d379205e22cb"
 )
 
 func main() {
@@ -30,6 +30,41 @@ func main() {
 		Scopes:       []string{oidc.ScopeOpenID, "profile", "email", "roles"},
 	}
 
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	state := "exemplo"
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, config.AuthCodeURL(state), http.StatusFound)
+	})
+
+	http.HandleFunc("/auth/callback", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Query().Get("state") != state {
+			http.Error(w, "State doesnt match", http.StatusBadRequest)
+			return
+		}
+
+		oauth2Token, err := config.Exchange(ctx, r.URL.Query().Get("code"))
+		if err != nil {
+			http.Error(w, "Problema ao trocar token", http.StatusInternalServerError)
+			return
+		}
+
+		rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+		if !ok {
+			http.Error(w, "Problema ao pegar id token", http.StatusInternalServerError)
+			return
+		}
+
+		res := struct {
+			OAuth2Token *oauth2.Token
+			IDToken     string
+		}{
+			oauth2Token, rawIDToken,
+		}
+
+		data, _ := json.MarshalIndent(res, "", "   ")
+		w.Write(data)
+
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
