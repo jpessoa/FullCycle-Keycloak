fe9ea83acd437795baa4fdf130f36c883ae60b36
Jefferson E. Pessoa - EasyNet Machine <jefferson@mile.com.br>
2021-07-26 10:02:22 -0400

    Até config 2

--- app1/main.go
+++ app1/main.go
@@ -1,10 +1,35 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/coreos/go-oidc"
+	oidc "github.com/coreos/go-oidc"
+	"golang.org/x/oauth2"
+)
+
 var (
 	clientID     = "app"
 	clientSecret = "..."
 )
 
 func main() {
+	ctx := context.Background()
+	provider, err := oidc.NewProvider(ctx, "http://localhost:8080/auth/realms/demo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Endpoint:     provider.Endpoint(),
+		RedirectURL:  "http://localhost:8081/auth/callback",
+		Scopes:       []string{oidc.ScopeOpenID, "profile", "email", "roles"},
+	}
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
 
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module keycloak-app
 
 go 1.13
+
+require (
+	github.com/coreos/go-oidc v2.2.1+incompatible
+	github.com/pquerna/cachecontrol v0.1.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210628180205-a41e5a781914
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
+)
